b030d23e4bf3d5c53dc3cac734ec7d395633034d
DEVGRuSix <maoy7594@gmail.com>
2023-08-11 20:09:58 +0800

    登录表单功能、邮箱验证码

---
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+                src="https://img1.baidu.com/it/u=4159158149,2237302473&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500"></el-image>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到南孔文化生态系统</div>
+      <div style="margin-top: 10px">在这里你可以了解有关南孔文化的一切知识</div>
+      <div style="margin-top: 5px">测试使用测试测试</div>
+
+    </div>
+    <div style="width: 400px;background-color: white">
+      <transition name="el-fade-in-linear">
+        <router-view/>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
---
+++ study-project-frontend/src/views/indexView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    欢迎进入南孔文化生态系统
+  </div>
+  <div>
+    <el-button @click="logout" type="danger" plain>退出登录</el-button>
+  </div>
+</template>
+
+<script setup>
+import {ElMessage} from "element-plus";
+import router from "@/router";
+import {get} from "@/net";
+
+const logout = () => {
+  get('/api/auth/logout',(message) => {
+    const successMessage = message || "退出成功";
+    ElMessage.success(successMessage)
+    router.push('/')
+  })
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
